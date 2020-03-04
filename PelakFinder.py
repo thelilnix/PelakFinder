a53ce426de851aa80419a635717fd6c431f3e8fc
= <=>
2020-03-04 20:17:54 +0330

    Program completed

---
+++ PelakFinder.py
@@ -0,0 +1,69 @@
+# -*- coding: UTF-8 -*-
+# Massachusetts Institute of Technology (MIT) .
+# An open source project created by Saman Ebrahimnezhad .
+# Created at Iran (IR) .
+# Python 3 .
+
+import sys
+import json
+
+from tkinter import *
+
+class PelakFinder:
+    def __init__(self):
+        
+        code = ""
+        alphabet = ""
+
+        if len(sys.argv) < 2:
+            print("ERROR: Syntax : python3 PelakFinder.py CAR_TAG_CODE CAR_TAG_ALPHABET_(optional)")
+            return
+        elif len(sys.argv) == 2:
+            code = sys.argv[1]
+        else:
+            code = sys.argv[1]
+            alphabet = sys.argv[2]
+
+        # JSON compilation and creation by Saman Ebrahimnezhad .
+        # Online_RESTFUL_API : https://api.jsonbin.io/b/5a577a6d7cfd5a4dbc6b3b19
+        
+        jsonFile = open("Pelaks.json", mode = 'r')
+        
+        text = jsonFile.read()
+
+        jsonFile.close()
+
+        data = json.loads(text)
+
+        labelText = "Not found"
+
+        if alphabet != "":
+            if alphabet in data[code][0].keys():
+                labelText = data[code][0][alphabet]
+            else:
+                labelText = data[code][0][code]
+        else:
+            labelText = data[code][0][code]
+
+        # ---------- Start GUI ----------
+
+        root = Tk()
+        root.title("PelakFinder")
+        
+        root.geometry("300x300")
+
+        self.message = Message(root, text = labelText, justify = "center")
+        self.message.config(font = ("times", 15, "bold"))
+
+        self.closeBtn = Button(root, width = 10, text = "Close", command = root.destroy)
+        
+        self.message.pack()
+        self.closeBtn.pack()
+
+        root.mainloop()
+
+def main():
+    PelakFinder()
+
+if __name__ == "__main__":
+    main()
